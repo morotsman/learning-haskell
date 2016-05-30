43f617d2601eaa5583640e6ea95bbd6bbb57968d
niklas.leopold <leopold.niklas@gmail.com>
2016-05-30 19:26:04 +0200

    learning haskell

--- basic_haskell/function_declaration.hs
+++ basic_haskell/function_declaration.hs
@@ -18,4 +18,37 @@ squareNumbers2 n = take n $ [x^2 | x <- [1..]]
 
 --list comprehension with filter and generators
 
---partialy applied function 
+triangels :: [(Int,Int,Int)]
+triangels = [(a,b,c) | c <- [1..], b <- [1..c], a <- [1..c], a^2 + b^2 == c^2]
+
+--pattern matching
+
+map' :: (a->b) -> [a] -> [b]
+map' f []     = []
+map' f (x:xs) = f(x) : map' f xs
+
+filter' :: (a->Bool) -> [a] -> [a]
+filter' f []    = []
+filter' f(x:xs) = if f(x) then x:filter' f xs else filter' f xs
+
+fib :: Int -> Int
+fib n | n == 0 = 0
+      | n == 1 = 1
+      | otherwise = fib(n-1) + fib(n-2)
+
+
+fib' :: Int -> Int
+fib' 0 = 0
+fib' 1 = 1
+fib' n = fib(n-1) + fib(n-2)
+
+
+fibs :: Int -> [Int]
+fibs n = [fib(x) | x <- [0..n]]
+
+--partialy applied function
+
+
+
+
+ 
